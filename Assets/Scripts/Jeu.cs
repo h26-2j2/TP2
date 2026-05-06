653dbcf50db659e2865a57e5f6e56fda5d1dd810
AnimatedPlays <129552246+AnimatedPlays@users.noreply.github.com>
2026-05-06 14:02:09 -0400

    Bug Fixes
    Fixed Timer reset
    
    Changed/Added variables
    
    What needs to be fixed is the bouton clicking and the question limit and transition to new level.
    
    once those are done, levels can start being made

--- Assets/Scripts/Jeu.cs
+++ Assets/Scripts/Jeu.cs
@@ -5,9 +5,10 @@ using UnityEngine.InputSystem;
 using Unity.VisualScripting;
 using UnityEngine.SceneManagement;
 using System.Threading.Tasks;
+using Unity.VisualScripting.Dependencies.NCalc;
 
 public class GenereEquation : MonoBehaviour
-{   
+{
     public int nombreDeQuestions = 5; // Nombre total de questions à poser
     public TMP_Text GameOverText; // Texte pour afficher le message de fin de jeu
     public TMP_Text GameWinText; // Texte pour afficher le message de victoire
@@ -15,6 +16,7 @@ public class GenereEquation : MonoBehaviour
     public TMP_Text reponse1; // Exemple de réponse 1
     public TMP_Text reponse2; // Exemple de réponse 2
     public TMP_Text reponse3; // Exemple de réponse 3
+
     public TMP_Text EquationTexte; // Texte pour afficher l'équation
     public TMP_Text texteTemps; // Texte pour afficher le temps restant
     public TMP_Text pointDeVieText; // Texte pour afficher les points de vie
@@ -23,7 +25,7 @@ public class GenereEquation : MonoBehaviour
     public int nombre2; // Deuxième nombre de l'équation
     public int pointDeVie = 3; // Exemple de points de vie
     public bool GameOver = false; // Variable pour suivre l'état de fin de jeu
-    
+    public bool EnAttente = false;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -68,65 +70,117 @@ public class GenereEquation : MonoBehaviour
 
         // Afficher les points de vie
         pointDeVieText.text = $"Points de vie : {pointDeVie}";
-        
 
-      
-        
 
-        
-        
-        
+        // initialiser le nombre de questions restantes
+        nombreDeQuestions = 5; // Par exemple, le joueur doit répondre à 5 questions pour gagner
+
+
+
+
+
+
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (tempsInitial > 0)
+        {
+            // Ajouter un temps limité pour répondre à l'équation
+            tempsInitial -= Time.deltaTime;
+            texteTemps.text = $"{tempsInitial:F1} S";
+        }
 
-        // Ajouter un temps limité pour répondre à l'équation
-        tempsInitial -= Time.deltaTime;
-        texteTemps.text = $"{tempsInitial:F1} S";
 
-
-        // lorsque le joueur clique sur une réponse, vérifier si c'est la bonne réponse, si c'est la bonne réponse, afficher un message de victoire et générer une nouvelle équation, sinon afficher un message d'échoue et pert une 1 vie
-        
-        
-        if (tempsInitial <= 0 && !GameOver) // si le temps atteint 0
+        if (tempsInitial <= 0 && !GameOver && EnAttente == false) // si le temps atteint 0
         {
             // Afficher un message d'échoue et pert une 1 vie
             EquationTexte.text = "Temps écoulé !";
-            pointDeVie--; // enleve une vie
+
             pointDeVieText.text = $"Points de vie : {pointDeVie}"; // Met à jour le texte des points de vie
-            
-            // Arrête le temps pour un couert instantt
             tempsInitial = 0f; // Assure que le temps ne devient pas négatif
+
+            // Arrête le temps pour un couert instantt
+            EnAttente = true;
+            pointDeVie--; // enleve une vie
 
             // Attendre un court instant avant de générer une nouvelle équation, réinitilaiser le temps et continuer le jeu
 
-            tempsInitial = 10f; // Réinitialiser le temps pour la prochaine équation
-            Invoke("Start", 2f); // Appelle la méthode Start après 2 secondes
-            
-            
+
+            Invoke("Start", 3f); // Appelle la méthode Start après 3 secondes
+            Invoke("ReinitialiserTemps", 3f);
+
+
 
         }
         if (pointDeVie <= 0) // si les points de vie atteignent 0
         {
             // Afficher un message de fin de jeu
-            GameOverText.text = "Game Over !";
+            GameOverText.text = "Partie Terminée !";
 
-            // Le temps s'arrête et le jeu ne génère plus de nouvelles équations
+            // Le temps s'arrête et le jeu ne génère plus de nouvelles équations et désactive les interactions du joueur
             enabled = false; // Désactive ce script pour arrêter les mises à jour
             GameOver = true; // Met à jour l'état de fin de jeu
         }
-        {
 
-            // si le joueur répond correctement à chacune des questions, afficher un message de victoire, arrêter le temps, attendre avant que le jeu change de scène ou niveau
-            if (nombreDeQuestions <= 0)
-            {
-                GameWinText.text = "Félicitations, vous avez gagné !";
-                enabled = false; // Désactive ce script pour arrêter les mises à jour
-                GameOver = true; // Met à jour l'état de fin de jeu
-                Task.Delay(2000).ContinueWith(_ => SceneManager.LoadScene("niveau2")); // Charge la scène suivante (niveau 2) après un délai de 2 secondes
-            }
-        
+        // si le joueur répond correctement à chacune des questions, afficher un message de victoire, arrêter le temps, attendre avant que le jeu change de scène ou niveau
+        if (nombreDeQuestions <= 0)
+        {
+            GameWinText.text = "Félicitations, vous avez gagné !";
+            enabled = false; // Désactive ce script pour arrêter les mises à jour
+            GameOver = true; // Met à jour l'état de fin de jeu
+            Task.Delay(2000).ContinueWith(_ => SceneManager.LoadScene("niveau2")); // Charge la scène suivante (niveau 2) après un délai de 2 secondes
+        }
     }
-}}
+
+    public void BoutonClique()
+    { // créer une fonction pour vérifier la réponse du joueur
+
+        // créer une fonction pour vérifier la réponse du joueur
+        if (reponse1.text == (nombre1 + nombre2).ToString()) // si la réponse1 est correcte
+        {
+            nombreDeQuestions--; // enleve une question du total
+            EquationTexte.text = "Bonne réponse !";
+            EnAttente = true;
+            Start(); // génère une nouvelle équation
+            ReinitialiserTemps();
+
+        }
+        else if (reponse2.text == (nombre1 + nombre2).ToString()) // si la réponse2 est correcte
+        {
+            nombreDeQuestions--; // enleve une question du total
+            EquationTexte.text = "Bonne réponse !";
+            EnAttente = true;
+            Start(); // génère une nouvelle équation
+            ReinitialiserTemps();
+
+        }
+        else if (reponse3.text == (nombre1 + nombre2).ToString()) // si la réponse3 est correcte
+        {
+            nombreDeQuestions--; // enleve une question du total
+            EquationTexte.text = "Bonne réponse !";
+            EnAttente = true;
+            Start(); // génère une nouvelle équation
+            ReinitialiserTemps();
+        }
+        else
+        {
+            // si la réponse est incorrecte, afficher un message d'erreur et perdre une vie
+            EquationTexte.text = "Mauvaise réponse !";
+            pointDeVie--; // enleve une vie
+            pointDeVieText.text = $"Points de vie : {pointDeVie}"; // Met à jour le texte des points de vie
+
+            // Attendre un court instant avant de générer une nouvelle équation, réinitilaiser le temps et continuer le jeu
+            EnAttente = true;
+            Invoke("Start", 3f); // Appelle la méthode Start après 2 secondes
+            Invoke("ReinitialiserTemps", 3f); // Réinitialiser le temps après 3 secondes
+        }
+    }
+
+    void ReinitialiserTemps()
+    {
+        EnAttente = false;
+        tempsInitial = 10f; // Réinitialiser le temps pour la prochaine équation
+    }
+}
